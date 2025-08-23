66e7c2ce8ed9f8ef2bf2b3c399c6ec7c42cb654d
Ajay Gandecha <ajgandecha@gmail.com>
2025-08-23 12:01:16 -0400

    chore: remove unused imports

--- server/scripts/setup_prod.py
+++ server/scripts/setup_prod.py
@@ -9,15 +9,10 @@ it will likely fail if the production environment is already set up)
 Usage: python3 -m server.scripts.setup_prod
 """
 
-from ..seeds.demo import insert_seed_data as insert_demo_seed
-from ..env import env
 from ..configuration import (
     setup_admin_db_cluster,
-    teardown_admin_db_cluster,
     setup_content_db_cluster,
-    teardown_content_db_cluster,
 )
-
 
 setup_admin_db_cluster()
 print("✅ Set up the admin db cluster")
