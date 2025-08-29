9ab5d2b72955cce2296978fa90692cabdce64611
Ajay Gandecha <ajgandecha@gmail.com>
2025-08-29 13:17:22 -0400

    chore: add env configuration to the web app
    Adds an env configuration to the web app so that the host is known.

--- web/utils/env.ts
+++ web/utils/env.ts
@@ -5,7 +5,6 @@ import { z } from "zod";
 const EnvType = z.object({
   MODE: z.enum(["development", "production"]).default("development"),
   HOST: z.string().default("localhost:8000"),
-  AUTH_MASTER_SECRET: z.string().default("REPLACE_ME"),
 });
 
 export const env = EnvType.parse(process.env);
